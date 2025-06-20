ae50a5e441405fbff9c85795a4a9b380df52eb7b
pramod7876 <gurungpramod981@gmail.com>
2025-06-20 14:55:17 +0545

    initial changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function greet(){
+ let box = document.getElementsByClassName("jstutor");
+ box[0].style.backgroundColor="red"
+ box[1].style.backgroundColor="green"
+ box[2].style.backgroundColor="yellow"
+ box[3].style.backgroundColor="skyblue"
+
+}
+
